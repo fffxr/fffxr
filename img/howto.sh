a8c86dd192246ebabab653ec2e444b533a6001f8
FFFXR <fffxr@yandex.com>
2019-09-06 22:02:25 +0200

    each page as PNG

--- img/howto.sh
+++ img/howto.sh
@@ -1,3 +1,3 @@
 # generate pages into png images
-pdftoppm -png GretaThunberg_song-by-The1975_translations_v04.pdf img/GretaThunberg_song-by-The1975_translations_v04.pdf_page
+pdftoppm -png GretaThunberg_song-by-The1975_translations_v05.pdf img/GretaThunberg_song-by-The1975_translations_v05.pdf_page
 
